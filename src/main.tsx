d705a834cefe90ccc1b583732300246c6794bfe0
bingzi <bingzi@example.com>
2025-07-17 17:50:41 +0800

    feat: Integrate Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 import './index.css'
 import App from './App.tsx'
+import { inject } from '@vercel/speed-insights';
+ 
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
